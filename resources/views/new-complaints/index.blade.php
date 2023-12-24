1e6f8ef3dd7d24cc8b54e252461577ce33b00212
farahnini <farahnini98@gmail.com>
2023-12-25 07:40:53 +0800

    Index new complaints

--- resources/views/new-complaints/index.blade.php
+++ resources/views/new-complaints/index.blade.php
@@ -2,33 +2,6 @@
 
 @section('content')
 
-    <!-- DataTables CSS -->
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.css">
-
-    <!-- jQuery -->
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-
-    <!-- DataTables JS -->
-    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.js"></script>
-
-
-    <script>
-        $(document).ready(function () {
-            // DataTable initialization
-            var table = $('#complaintsTable').DataTable({
-                "paging": true,
-                "ordering": true,
-                "info": true,
-                "searching": true, // Enable global search
-                "drawCallback": function (settings) {
-                    // Show the table after DataTables has been fully initialized
-                    $('#complaintsTable').show();
-                }
-            });
-        });
-    </script>
-
-
     <div class="app-main flex-column flex-row-fluid" id="kt_app_main">
         <div class="d-flex flex-column flex-column-fluid">
             <div id="kt_app_content" class="app-content flex-column-fluid">
@@ -72,18 +45,10 @@
 
                         <!--begin::Card body-->
                         <div class="card-body py-4">
-                            <!-- Button to create complaint-->
-                            <div class="mb-3 d-flex">
-                                <div class="ms-auto">
-                                    <a href="{{ route('complaints.create') }}" class="btn btn-sm btn-primary">
-                                        <i class="fas fa-plus"></i> Aduan
-                                    </a>
-                                </div>
-                            </div>
 
                             <!-- Create table all complaint-->
                             <div class="table-responsive">
-                                <table class="table table-bordered table-hover small" id="complaintsTable" style="display: none;">
+                                <table class="table table-bordered table-hover small">
                                     <thead class="thead-light">
                                         <tr>
                                             <th scope="col text-nowrap">No.</th>
