5b3a638b67e7cd95c8b29a3b033bc0129516c18e
farahnini <farahnini98@gmail.com>
2023-12-25 07:45:39 +0800

    Card index new complaints

--- resources/views/new-complaints/index.blade.php
+++ resources/views/new-complaints/index.blade.php
@@ -46,45 +46,47 @@
                         <!--begin::Card body-->
                         <div class="card-body py-4">
 
-                            <!-- Create table all complaint-->
-                            <div class="table-responsive">
-                                <table class="table table-bordered table-hover small">
-                                    <thead class="thead-light">
-                                        <tr>
-                                            <th scope="col text-nowrap">No.</th>
-                                            <th scope="col">No Aduan</th>
-                                            <th scope="col">Tarikh</th>
-                                            <th scope="col">Pelapor</th>
-                                            <th scope="col">Blok</th>
-                                            <th scope="col">Kategori</th>
-                                            <th scope="col">Tindakan</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        @foreach($complaints as $complaint)
-                                        <tr>
-                                            <th scope="row">{{ $loop->iteration }}</th>
-                                            <th scope="row">{{ $complaint->report_no ?? null }}</th>
-                                            <td style="white-space: nowrap;">
-                                                {{ \Carbon\Carbon::parse($complaint->created_at)->format('d-m-Y') }} <br>
-                                                ({{ \Carbon\Carbon::parse($complaint->created_at)->diffForHumans() }})
-                                            </td>
-                                            <td style="white-space: nowrap;">
-                                                {{ $complaint->user->name }}<br>
-                                                {{ $complaint->user->mobile_no }}
-                                            </td>
-                                            <td>{{ $complaint->block }}</td>
-                                            <td>
-                                                {{ $complaint->category->name }}<br>
-                                                {{ $complaint->location }}
-                                            </td>
-                                            <td class="text-center">
-                                                <a href="{{ route('new-complaints.edit', $complaint) }}" class="btn btn-sm btn-warning" style="padding: 5px;">Assign to &rarr;</a>
-                                            </td>
-                                        </tr>
-                                        @endforeach
-                                    </tbody>
-                                </table>
+                            <div class="row">
+                                @foreach($complaints as $complaint)
+                                    <div class="col-md-4 mb-3">
+                                        <div class="card">
+                                            <div class="card-header">
+                                                <div class="d-flex justify-content-between align-items-center">
+                                                    <h5>
+                                                        <i class="fas fa-file"></i> {{ $complaint->report_no ?? null }}
+                                                    </h5>
+                                                </div>    
+                                            </div>
+                                            <div class="card-body">
+                                                <p class="card-text">
+                                                    <strong>Tarikh:</strong> {{ \Carbon\Carbon::parse($complaint->created_at)->format('d-m-Y') }}<br>
+                                                    ({{ \Carbon\Carbon::parse($complaint->created_at)->diffForHumans() }})
+                                                </p>
+                                                <p class="card-text">
+                                                    <strong>Pelapor:</strong> {{ $complaint->user->name }}<br>
+                                                    {{ $complaint->user->mobile_no }}
+                                                </p>
+                                                <p class="card-text">
+                                                    <strong>Blok:</strong> {{ $complaint->block }}
+                                                </p>
+                                                <p class="card-text">
+                                                    <strong>Kategori:</strong> {{ $complaint->category->name }}
+                                                </p>
+                                                <p class="card-text">
+                                                    <strong>Lokasi:</strong> {{ $complaint->location}}                                                </p>
+                                                <p class="card-text">
+                                                    <strong>Deskripsi:</strong><br>
+                                                    {{ $complaint->description }}
+                                                </p>
+                                                <div class="text-center">
+                                                    <a href="{{ route('new-complaints.edit', $complaint) }}" class="btn btn-sm btn-warning" style="padding: 5px;">Assign to &rarr;</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                @endforeach
+                            </div>
+
                         </div>
                         <!--end::Card body-->
                     </div>
