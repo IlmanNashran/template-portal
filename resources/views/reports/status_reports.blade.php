d91ccd48039b0f08491a1aefe7811375ec89dc33
farahnini <farahnini98@gmail.com>
2023-12-25 06:34:55 +0800

    Responsive table

--- resources/views/reports/status_reports.blade.php
+++ resources/views/reports/status_reports.blade.php
@@ -35,43 +35,45 @@
 
                         <!--begin::Card body-->
                         <div class="card-body py-4">
-                            <table class="mt-5 table table-sm table-bordered">
-                                <thead>
-                                    <tr>
-                                        <th class="table-success">Nama petugas</th>
-                                        <th class="table-success">Peranan</th>
-                                        <th class="text-center table-success">Baharu</th>
-                                        <th class="text-center table-success">Dijawab</th>
-                                        <th class="text-center table-success">Selesai</th>
-                                        <th class="text-center table-success">Dinilai</th>
-                                        <th class="text-center table-success">KIV</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @foreach ($users_status_count as $user)
+                            <div class="mt-2 table-responsive">
+                                <table class="mt-5 table table-sm table-bordered">
+                                    <thead>
                                         <tr>
-                                            <td>{{ $user->name }}</td>
-                                            <td>{{ $user->role }}</td>
-                                            <td class="text-center">{{ $user->Baharu }}</td>
-                                            <td class="text-center">{{ $user->Dijawab }}</td>
-                                            <td class="text-center">{{ $user->Selesai }}</td>
-                                            <td class="text-center">{{ $user->Dinilai }}</td>
-                                            <td class="text-center">{{ $user->KIV }}</td>
+                                            <th class="table-success">Nama petugas</th>
+                                            <th class="table-success">Peranan</th>
+                                            <th class="text-center table-success">Baharu</th>
+                                            <th class="text-center table-success">Dijawab</th>
+                                            <th class="text-center table-success">Selesai</th>
+                                            <th class="text-center table-success">Dinilai</th>
+                                            <th class="text-center table-success">KIV</th>
                                         </tr>
-                                    @endforeach
-                                    
-                                </tbody>
-                                <tfoot>
-                                    <tr>
-                                        <th colspan="2" class="text-center table-success">Jumlah</th>
-                                        <th class="text-center table-success">{{ $users_status_count->sum('Baharu') }}</th>
-                                        <th class="text-center table-success">{{ $users_status_count->sum('Dijawab') }}</th>
-                                        <th class="text-center table-success">{{ $users_status_count->sum('Selesai') }}</th>
-                                        <th class="text-center table-success">{{ $users_status_count->sum('Dinilai') }}</th>
-                                        <th class="text-center table-success">{{ $users_status_count->sum('KIV') }}</th>
-                                    </tr>
-                                </tfoot>
-                            </table>
+                                    </thead>
+                                    <tbody>
+                                        @foreach ($users_status_count as $user)
+                                            <tr>
+                                                <td>{{ $user->name }}</td>
+                                                <td>{{ $user->role }}</td>
+                                                <td class="text-center">{{ $user->Baharu }}</td>
+                                                <td class="text-center">{{ $user->Dijawab }}</td>
+                                                <td class="text-center">{{ $user->Selesai }}</td>
+                                                <td class="text-center">{{ $user->Dinilai }}</td>
+                                                <td class="text-center">{{ $user->KIV }}</td>
+                                            </tr>
+                                        @endforeach
+                                        
+                                    </tbody>
+                                    <tfoot>
+                                        <tr>
+                                            <th colspan="2" class="text-center table-success">Jumlah</th>
+                                            <th class="text-center table-success">{{ $users_status_count->sum('Baharu') }}</th>
+                                            <th class="text-center table-success">{{ $users_status_count->sum('Dijawab') }}</th>
+                                            <th class="text-center table-success">{{ $users_status_count->sum('Selesai') }}</th>
+                                            <th class="text-center table-success">{{ $users_status_count->sum('Dinilai') }}</th>
+                                            <th class="text-center table-success">{{ $users_status_count->sum('KIV') }}</th>
+                                        </tr>
+                                    </tfoot>
+                                </table>
+                            </div>                     
                         </div>
                         <!--end::Card body-->
                     </div>
